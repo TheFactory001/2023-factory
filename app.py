c67ba3f93ff88bd67f74b6b799892c77e4b3b432
TheFactory001 <98657707+TheFactory001@users.noreply.github.com>
2023-03-04 08:46:15 -0800

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-name = "  babalola samuel"
-print(name.upper())
-print(name.lower())
-print(name.strip())
-print(name.find("sam"))
-print(name.replace("a", "e"))
-print("sam" in name)
-print("she" not in name)
