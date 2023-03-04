58dceae14c30ef14069e32ba8af13a26bb3778e0
Esther1605 <125884386+Esther1605@users.noreply.github.com>
2023-03-04 17:54:15 +0000

    Section3 Assessement

---
+++ section3a.py
@@ -0,0 +1,6 @@
+# write a program to display "Hello" if number entered is a multiple of five otherwise print "Bye"
+num = int(input("Enter your age:"))
+if num % 5 == 0:
+    print("Hello")
+else:
+    print("Bye")
