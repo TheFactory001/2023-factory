0dd801202f4d6088140153a7c8f9ea4b771dfc89
Esther1605 <125884386+Esther1605@users.noreply.github.com>
2023-03-06 19:37:54 +0000

    Add files via upload

---
+++ section3b.py
@@ -0,0 +1,8 @@
+# function with two arguments th adds 5 to the two argument and returns the total sum
+def calculate_total_sum(value, arg):
+    total_sum = 0
+    for number in (value, arg):
+        total_sum += number+5
+
+
+print("total_sum")
