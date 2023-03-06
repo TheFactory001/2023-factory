a4035c57eb488a603f135877dc8463149b489edf
Esther1605 <125884386+Esther1605@users.noreply.github.com>
2023-03-06 19:38:56 +0000

    Add files via upload

---
+++ section3c.py
@@ -0,0 +1,7 @@
+# write a code that loops through a list of numbers to determine the greatest
+list1 = [20, 30, 45, 86, 67, 54, 66]
+list1.sort()
+print(list1)
+print("Greatest element is:", list1[-1])
+print(list1[0])
+print(list1[6])
