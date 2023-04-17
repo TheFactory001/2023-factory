4ddfef1a7bdf2656663e151a4c01159e70b3e4bf
Esther1605 <125884386+Esther1605@users.noreply.github.com>
2023-04-17 08:34:23 +0100

    Delete consonant.py

--- consonant.py
+++
@@ -1,7 +0,0 @@
-consonant = "bcdfghjklmnpqrstvwxyz"
-letter = input("enter any letter: ")
-#input(letter = "o")
-if letter in consonant:
-    print("Letter is consonant")
-else:
-    print("Letter is not consonant") 
