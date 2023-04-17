ce22b27630bc63d1bae32db0c6cecb6a683402e2
Esther1605 <125884386+Esther1605@users.noreply.github.com>
2023-04-17 08:30:07 +0100

    Delete add_target.py

--- add_target.py
+++
@@ -1,9 +0,0 @@
-def add_target(mylist, target):
-    if target in mylist:
-        print("Target already in list")
-    else:
-        mylist.append(target)
-        print(mylist)        
-
-mylist = ["a", "g", "h"]
-add_target(mylist, "h")      
