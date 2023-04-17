725c1f658a7eb6d810f56d0e785457460dd35b24
Esther1605 <125884386+Esther1605@users.noreply.github.com>
2023-04-17 08:34:45 +0100

    Delete convert_temperature.py

--- convert_temperature.py
+++
@@ -1,7 +0,0 @@
-#Write a python program to convert temperatures from celsius to farenheit
-#def convert(celsius, rate):
-   # rate = (18 * celsius) + 32
-  #  farenheit = celsius * rate
- #   return farenheit
-
-#convert(100)    
