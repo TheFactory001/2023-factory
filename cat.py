cb71ba38da45fbd7bda56cd6c9f61eeb23b9a777
TheFactory001 <98657707+TheFactory001@users.noreply.github.com>
2023-07-29 09:38:28 -0700

    Delete cat.py

--- cat.py
+++
@@ -1,7 +0,0 @@
-# given two integers, return their product only if the product is greater than 1000, else return thei sum.
-x = 23
-y = 12
-if x * y > 1000:
-    print(x * y)
-else:
-    print(x + y)
